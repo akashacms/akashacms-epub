7e69d86730cd3d285ba20b550105f16e185bea1f
David Herron <david@davidherron.com>
2015-04-17 17:07:29 -0700

    Resolve some error messages due to unnecessarily retrieved data

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -3,13 +3,13 @@ module.exports = function(grunt) {
     grunt.registerTask('bundleEPUB', function() {
         var done = this.async();
         grunt.config.requires('akasha');
-        grunt.config.requires('config');
+        // grunt.config.requires('config');
         var akasha = grunt.config('akasha');
-        var config = grunt.config('config');
+        // var config = grunt.config('config');
         var epubversion = "epub3";
         if (this.flags.epub2) epubversion = "epub2";
         else if (this.flags.epub3) epubversion = "epub3";
-        akasha.plugin('akashacms-epub').bundleEPUB(config, epubversion, function(err) {
+        akasha.plugin('akashacms-epub').bundleEPUB(epubversion, function(err) {
             if (err) done(err);
             else done();
         });
